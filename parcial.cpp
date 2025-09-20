ee1f4856a20a958c549387212e5f6aca25d5533b
jesusvargasc <estivenvargas0412@gmail.com>
2025-09-20 09:51:48 -0500

    agrego actuación

--- parcial.cpp
+++ parcial.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 #include <cmath>
-namespace std;
 int leernotas(){
-    char f,o;
-    cout<<"Desea ingresar manualmente? s/n";
-    cin>> f;
-    if (f == s) {
-        int n;
-        cout<<"¿Cuántos estudiantes vas a ingresar?";
-        
+    int n = 0;
+    const int MAX_ESTUDIANTES = 100;
+    float notas[MAX_ESTUDIANTES];
+
+    char f;
+    std::cout << "Desea ingresar manualmente? s/n";
+    std::cin >> f;
+    if (f == 's'){
+        std::cout << "¿Cuántos estudiantes vas a ingresar?";
+        std::cin >> n;
+        if(n > MAX_ESTUDIANTES) n = MAX_ESTUDIANTES;
+        for (int i = 0; i < n; i++){
+            std::cout << "Ingrese la nota del estudiante " << i + 1 << ": ";
+            std::cin >> notas[i];
+        }
+    } else {
+        n = 5;
+        notas[0] = 5.0;
+        notas[1] = 4.2;
+        notas[2] = 3.3;
+        notas[3] = 2.0;
+        notas[4] = 1.0;
     }
-    notas[][]={]
+    // Aquí puedes retornar o procesar las notas según lo que necesites
+    return 0;
+}
 //int calcularprom(){
 
 
