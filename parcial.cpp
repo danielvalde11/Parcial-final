e7e76ff5c7fea7954fe244a0098a3860c6ae7dfd
danielvalde11 <dl449048@gmail.com>
2025-09-20 14:27:44 +0000

    Parcial final

---
+++ parcial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+namespace std;
+int leernotas(){
+    char f,o;
+    cout<<"Desea ingresar manualmente? s/n";
+    cin>> f;
+    if (f == s) {
+        int n;
+        cout<<"¿Cuántos estudiantes vas a ingresar?";
+        
+    }
+    notas[][]={]
+//int calcularprom(){
+
+
